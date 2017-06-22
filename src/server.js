b9398ecc832038c1711e784cf020df048f8fe7c5
Adam Alcott <aalcott14@gmail.com>
2017-06-22 12:58:52 -0500

    change app.use route to correct node modules

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true,
 }));
-app.use(express.static(path.join(__dirname, '../node_modules')));
+app.use(express.static(path.join(__dirname, './node_modules')));
 app.use(express.static(path.join(__dirname, '/client')));
 app.use('/api', router);
 
